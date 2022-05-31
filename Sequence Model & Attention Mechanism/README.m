6ab2cd6c3a5f2e4871038bb4fe0f9d5bfa6f1876
SANSKAR PANDEY <78846892+PANDEYSANSKAR@users.noreply.github.com>
2022-05-31 14:08:50 +0530

    Update README.m

--- Sequence Model & Attention Mechanism/README.m
+++ Sequence Model & Attention Mechanism/README.m
@@ -1,1 +1,23 @@
+1) Trigger Word Detection
+Welcome to the second and last programming assignment of Week 3!
 
+In this week's videos, you learned about applying deep learning to speech recognition. In this assignment, you will construct a speech dataset and implement an algorithm for trigger word detection (sometimes also called keyword detection, or wake word detection).
+
+Trigger word detection is the technology that allows devices like Amazon Alexa, Google Home, Apple Siri, and Baidu DuerOS to wake up upon hearing a certain word.
+For this exercise, our trigger word will be "activate". Every time it hears you say "activate", it will make a "chiming" sound.
+By the end of this assignment, you will be able to record a clip of yourself talking, and have the algorithm trigger a chime when it detects you saying "activate".
+After completing this assignment, perhaps you can also extend it to run on your laptop so that every time you say "activate" it starts up your favorite app, or turns on a network connected lamp in your house, or triggers some other event?
+
+In this assignment you will learn to:
+
+Structure a speech recognition project
+Synthesize and process audio recordings to create train/dev datasets
+Train a trigger word detection model and make predictions
+
+
+2)Neural Machine Translation
+Welcome to your first programming assignment for this week!
+
+You will build a Neural Machine Translation (NMT) model to translate human-readable dates ("25th of June, 2009") into machine-readable dates ("2009-06-25").
+You will do this using an attention model, one of the most sophisticated sequence-to-sequence models.
+This notebook was produced together with NVIDIA's Deep Learning Institute.
